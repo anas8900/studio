9fe7e57606a66b58206bada5fcd41bde9077695f
Anas Raza <anasraza5617717@gmail.com>
2024-08-03 18:55:27 +0500

    ssa

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,3 +76,59 @@ function month(){
 }
 month()
 
+
+function init(){
+    
+    gsap.registerPlugin(ScrollTrigger);
+    
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector(".main"),
+      smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+    
+    ScrollTrigger.scrollerProxy(".main", {
+      scrollTop(value) {
+        return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      }, 
+      getBoundingClientRect() {
+        return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+      },
+      pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
+    
+    
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    
+    ScrollTrigger.refresh();
+    
+}
+init()
+
+function page1anim(){
+    
+gsap.from(".content h1", {
+    y:-40,
+    duration:1,
+    stagger:2,
+    opacity:0
+})
+gsap.to(".content p", {
+    y:-10,
+    duration:1,
+    stagger:2,
+    opacity:1
+})
+gsap.from(".arrow",{
+    x:70,
+    duration:1,
+    opacity:0
+})  
+gsap.from(".navbar-nav li, .btn, .navbar-brand",{
+    y:-20,
+    duration:1,
+    stagger:0.1,
+    opacity:0
+})
+}
+page1anim()
